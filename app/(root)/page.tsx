6aa24819411aaa85ac89ce6ee0879d9da0acad07
Finley <finley.mwachia12@gmail.com>
2024-07-05 09:32:35 +0300

    create HeaderBox file and added rafce function

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">Welcome,Finley</header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
---
+++ components/HeaderBox.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeaderBox = () => {
+  return (
+    <div>HeaderBox</div>
+  )
+}
+
+export default HeaderBox
